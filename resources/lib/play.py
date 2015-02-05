64ecb994a9eaff5b564cd40fdf1882a4412b1758
nunoidev <nuno.carvalheiro87@outlook.com>
2015-02-06 00:12:44 +1100

    Added subtitles support to XBMC/Kodi
    Brightcove uses DFXP subtitles, added the option fetch and convert to
    SRT.

--- resources/lib/play.py
+++ resources/lib/play.py
@@ -18,9 +18,13 @@
 #
 
 import sys
+import os	#need to write temp subtitle
+import re	#need regular expression to read subtitle file
+import urllib2	#need to fetch the subtitle
 
 import xbmc
 import xbmcgui
+import xbmcaddon	#need to get addon info/profile
 
 import config
 import utils
@@ -38,8 +42,33 @@ def play(url):
         if hasattr(listitem, 'addStreamInfo'):
             listitem.addStreamInfo('audio', p.get_xbmc_audio_stream_info())
             listitem.addStreamInfo('video', p.get_xbmc_video_stream_info())
+
+        #get addon profile/path to create a temporary .srt file
+	addon = xbmcaddon.Addon()
+        profile = addon.getAddonInfo('profile')
+        subfile = xbmc.translatePath(os.path.join(profile, 'temp.srt'))
+        prodir  = xbmc.translatePath(os.path.join(profile))
+        if not os.path.isdir(prodir):
+            os.makedirs(prodir)
+
+        #fetch the subtitle and convert to srt
+	pg = urllib2.urlopen(p.subtitles).read()
+        if pg != "":
+            ofile = open(subfile, 'w+')
+            captions = re.compile('<p begin="(.+?)" end="(.+?)">(.+?)</p>').findall(pg)
+            idx = 1
+            for cstart, cend, caption in captions:
+                cstart = cstart.replace('.',',')
+                cend   = cend.replace('.',',').split('"',1)[0]
+                caption = caption.replace('<br/>','\n').replace('&gt;','>')
+                ofile.write( '%s\n%s --> %s\n%s\n\n' % (idx, cstart, cend, caption))
+                idx += 1
+            ofile.close()
     
-        utils.log("Attempting to play: %s" % p.get_title())
+    	#tell the player to read the subtitle
+	listitem.setSubtitles(['special://temp/temp.srt', subfile])
+        
+	utils.log("Attempting to play: %s" % p.get_title())
         xbmc.Player().play(p.get_url(), listitem)
     except:
         utils.handle_error("Unable to play video")
